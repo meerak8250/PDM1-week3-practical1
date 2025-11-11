f2f64661159b50a499af6ec8a95d9e06be0a8881
Meera Kofkin <dsx522@york.ac.uk>
2025-11-11 20:47:20 +0000

    done

--- exercise2-1/sketch.js
+++ exercise2-1/sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let fillColour;
 let squareXCoord=300;
 let squareYCoord=300;
@@ -37,39 +36,3 @@ function draw(){
         }    
         }
 }
-=======
-let fillColor;
-let x=300;
-let y=300;
-function setup(){
-    
-    fillColor= color(random(255),random(255), random(255));
-createCanvas(600,600);
-}
-function draw(){
-    background(0);
-    fill(fillColor);
-circle(x,y,300);
-}
-function keyPressed() {
- 
-if (key ==='w' || keyCode===UP_ARROW){
-    y-= 5;
-} if (key ==='s'|| keyCode===DOWN_ARROW){
-    y+= 5;
-} if (key ==='a'|| keyCode===LEFT_ARROW){
-    x-= 5;
-} if (key ==='d'||keyCode===RIGHT_ARROW){
-    x += 5;
-}
-}
-
-
-
-
-
-
-
-
-   
->>>>>>> ac2372b21826ddd9f9fcfde609fbbdc021a43f12
--- exercise2-3/sketch.js
+++ exercise2-3/sketch.js
@@ -1,29 +1,32 @@
-let x=300
-let y=300
-let fillColor= color(random(255),random(255), random(255));
-let shapeSelect=true
+let shapeSelected= false;
+let x= 200;
+let y= 150;
 
 function setup(){
-createCanvas(600,600);
-fillColor= color(random(255),random(255), random(255));
+    createCanvas(600,600);
+    rectMode(CENTER);
+    fill(255);
 }
 
 function draw(){
     background(0);
-    fill(255);
-    rect(x,y,300,200);
-
-    function mousePressed(){
-        if(mouseX<300)
-    }
-
-
-
-
-
-
-
-
-
-
+    rect(x, y, 300, 200);
 }
+
+function mousePressed(){
+    if(mouseX >= x - 150 - && mouseX <= x + 150 && mouseY >= y -100 && mouseY <= y + 100){
+        shapeSelected= true;
+        fill(150,0,175);
+    }
+}
+function mouseDragged(){
+    if(shapeSelected){
+        x = mouseX;
+        y = mouseY;
+    }
+function mouseReleased(){
+    shapeSelected= false;
+    fill(255);
+}    
+}
+
